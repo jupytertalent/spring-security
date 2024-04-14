9f4438438e9441fb3f337a3fa8deb786b1c6015d
Ojhatrilo <ojhatrilochan5@gmail.com>
2024-04-14 12:56:21 +0530

    roles updated

--- src/main/java/com/example/Spring_security_test/controller/TestController.java
+++ src/main/java/com/example/Spring_security_test/controller/TestController.java
@@ -1,10 +1,7 @@
 package com.example.Spring_security_test.controller;
 
-import org.springframework.context.annotation.Role;
-import org.springframework.security.access.prepost.PostAuthorize;
-import org.springframework.security.access.prepost.PreAuthorize;
+
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
